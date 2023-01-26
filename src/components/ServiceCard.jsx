208d03bf61044910e5305198f5b07fac4a47d857
Nunzio Baldares <bvldares@outlook.com>
2023-01-26 13:07:47 +0100

    hover state fix

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
 import "../css/serviceCard.css"
 
 export default function ServiceCard(props){
+
+    const[hovering, setHovering] = useState(false)
+    
+
+
+
+
+
     return (
-        <div className="service-card">
+        <div className="service-card" onMouseEnter={()=>setHovering(true)} onMouseLeave={()=>setHovering(false)}>
             <img className="service-card-img" src={props.img} alt="project img"/>
-            <div className="service-card-intro">
-                <h3 className="service-card-title">{props.title}</h3>
-                <p className="service-card-body">{props.body}</p>
+            <div className="service-card-intro" style={{backgroundColor: hovering ? "#E57E69" : ""}}>
+                <h3 className="service-card-title" style={{color: hovering ? "#fff" :""}}>{props.title}</h3>
+                <p className="service-card-body" style={{color: hovering ? "#fff" :""}}>{props.body}</p>
             </div>
         </div>
     )
